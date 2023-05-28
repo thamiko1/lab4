d7572fbece3f349f325ce166992c5381a44f107e
EdgedSquirrels <daniel03044@gmail.com>
2023-05-29 03:22:16 +0800

    room split

--- game.js
+++ game.js
@@ -1,99 +1,105 @@
-//
-// BASIC
-//
 import express from 'express'
+import session from 'express-session'
 import http from 'http'
 import { Server } from 'socket.io'
 import * as fs from 'fs'
-
-var app = express();
-var server = http.createServer(app);
-var io = new Server(server);
-var nicknames = [];
-
 import path from 'path';
 import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-server.listen(3333);
-
-// Use the public directory to access files
-app.use("", express.static(__dirname + '/public'));
-
-app.get("/", function (req, res) {
-    // This is just a test for menu, so it will redirect to menu immediately.
-    res.redirect('/menu')
-})
-
 import vocab from './public/new.json' assert {type: 'json'}
+
+
+var new_userID = 0, new_roomID = 0;
+
+//
+// SOME CLASSES
+//
+
+class UserProfile {
+    constructor (userID, max_user, topic) {
+        // userID   (int): the user's ID
+        // max_user (int): the number of people the user wants to play 
+        // topic    (str): the user's selected topic
+        this.userID = userID;
+        this.max_user = max_user;
+        this.topic = topic;
+        this.hp = 100;
+        this.question_id = 1;
+    }
+}
+
+class RoomProfile {
+    constructor (roomID, max_user, topic) {
+        // roomID   (int): the room's ID
+        // max_user (int): the maximum number of people when staring the game 
+        // topic    (str): the room's topic
+        this.roomID = roomID;
+        this.max_user = max_user;
+        this.topic = topic;
+        this.users = new Object(); // users[userID] = userProfile
+
+        this.questions = build_questions(topic);
+        this.boss_hp = 100;
+    }
+
+    get num_user() {
+        return Object.keys(this.users).length;
+    }
+
+    get canStart() {
+        return (this.num_user >= this.max_user);
+    }
+
+    addUser(userID, max_user, topic) {
+        this.users[userID] = new UserProfile(userID, max_user, topic);
+    }
+}
+
+class RoomManager {
+    constructor() {
+        this.pending_rooms = new Object(); // pending_rooms[topic] = roomID
+        this.user2room = new Object();     // user2room[userID] = roomID
+        this.rooms = new Object();         // rooms[roomID] = roomProfile
+        // this.users = new Object();         // users[userID] = userProfile
+    }
+
+    addUser(userID, max_user, topic) {
+        let roomID = -1;
+        if (max_user == 1 || !(topic in this.pending_rooms)) {
+            if (max_user > 1) this.pending_rooms[topic] = new_roomID;
+            this.rooms[new_roomID] = new RoomProfile(new_roomID, max_user, topic);
+            roomID = new_roomID++;
+        }
+        else roomID = this.pending_rooms[topic];
+        this.user2room[userID] = roomID;
+
+        let room = this.rooms[roomID]; // reference(?
+        room.addUser(userID, max_user, topic);
+
+        if (room.num_user >= room.max_user) {
+            // Delete the pending status
+            if (max_user > 1) delete this.pending_rooms[topic];
+        }
+
+        return roomID;
+    }
+
+}
+
+
+//
+// SOME FUNCTIONS
+//
 
 function getRandom(min, max) {
     // Sample from [min, max)
     return Math.floor(Math.random() * (max - min)) + min;
 };
 
-
-//
-// MENU
-//
-
-app.get("/menu", function (req, res) {
-    res.sendFile(__dirname + '/public/menu.html');
-});
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-var player_category = "school", player_mode;
-
-app.post("/menu_start", function (req, res) {
-    console.log("receive start respose.");
-    console.log(req.body);
-    player_category = req.body.topic;
-    player_mode = req.body.mode;
-    console.log(player_mode)
-    if(player_mode == "single-player")
-        res.redirect("/game");
-    else
-        res.redirect("/pending");
-});
-
-//
-// pending
-//
-app.get('/pending', function (req, res) {
-    res.sendFile(__dirname + '/public/pending.html');
-});
-
-
-
-// 
-// GAME
-//
-
-// Handle the GET request with "/game" URL suffix
-app.get('/game', function (req, res) {
-    // Send gaming.html to client
-    res.sendFile(__dirname + '/public/game.html');
-
-    // build new questions based on the category
-    questions = build_questions(player_category, vocab);
-});
-
-var questions = build_questions(player_category, vocab), question_id = 0;
-var squirrel_hp = 100, point_hp = 100;
-
-function build_questions(category = "school", vocab) {
+function build_questions(category = "school") {
     let vocab_new = vocab.filter(word => word['category'].includes(category));
     vocab_new.sort(() => Math.random() - 0.5); // shuffle
     let chinese_to_english = {};
 
-    /*
-    for(i=0; i< 10; i++)
-        console.log(vocab_new[i]);
-    */
     for (let word of vocab_new) {
         for (let def of word["definition"]) {
             if (chinese_to_english[def] == undefined) chinese_to_english[def] = Array();
@@ -102,19 +108,11 @@ function build_questions(category = "school", vocab) {
     }
     /*
     chinese_to_english = {
-            "作業": [assignment, homework],
-            ...
-        }
-    cnt = 0;
-    for (let [k, v] of Object.entries(chinese_to_english)) {
-        console.log(k + ":" + v);
-        cnt++;
-        if (cnt >= 5) break;
+        "作業": [assignment, homework],
+        ...
     }
     */
 
-
-    // console.log(vocab_new[0]);
     let questions = [];
     for (let word of vocab_new) {
         let definition = word["definition"][getRandom(0, word["definition"].length)];
@@ -135,12 +133,6 @@ function build_questions(category = "school", vocab) {
     }
 
     /*
-    cnt = 0;f
-    for (let q of questions) {
-        console.log(q);
-        cnt++;
-        if (cnt >= 5) break;
-    }
     questions = [
         {
             definition: '功課',
@@ -153,8 +145,87 @@ function build_questions(category = "school", vocab) {
     return questions;
 }
 
-function send_question(socket) {
-    let question = questions[question_id];
+
+//
+// SETUP
+//
+
+var app = express();
+var server = http.createServer(app);
+var io = new Server(server);
+var manager = new RoomManager();
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+server.listen(3333);
+
+const sessionMiddleware = session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+});
+
+// Use the public directory to access files
+app.use("", express.static(__dirname + '/public'));
+app.use(sessionMiddleware);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+io.engine.use(sessionMiddleware);
+
+
+app.get("/", function (req, res) {
+    // This is just a test for menu, so it will redirect to menu immediately.
+    req.session.userID = new_userID++;
+    res.redirect('/menu');
+})
+
+
+//
+// MENU
+//
+
+app.get("/menu", function (req, res) {
+    res.sendFile(__dirname + '/public/menu.html');
+});
+
+// handle input form
+app.post("/menu_start", function (req, res) {
+    console.log("receive start respose.");
+    console.log(req.body);
+
+    req.session.max_user = (req.body.mode == "single-player" ? 1 : 3);
+    req.session.topic = req.body.topic;
+    res.redirect("/game");
+});
+
+//
+// pending
+//
+
+app.get('/pending', function (req, res) {
+    console.log(req.session.userID);
+    res.sendFile(__dirname + '/public/pending.html');
+});
+
+
+// 
+// GAME
+//
+
+// Handle the GET request with "/game" URL suffix
+app.get('/game', function (req, res) {
+    let roomID = manager.addUser(req.session.userID, req.session.max_user, req.session.topic);
+    req.session.roomID  = roomID;
+    console.log(req.session);
+    // Go to experimental game2
+    res.sendFile(__dirname + '/public/game2.html');
+});
+
+
+function send_question(socket, room, userID) {
+    let question = room.questions[room.users[userID].question_id];
     socket.emit("new question", question["definition"], question["options"]);
 }
 
@@ -182,33 +253,65 @@ function send_question(socket) {
 
 // const fs = require("fs");
 io.sockets.on("connection", function (socket) {
+
+    // FOR PENDING:
+
+    // handle new request
+    console.log("socket session:", socket.request.session);
     console.log("New user");
-    squirrel_hp = 100, point_hp = 100;
-    send_question(socket);
+    let roomID = socket.request.session.roomID, userID = socket.request.session.userID;
+    let room = manager.rooms[roomID];
+
+    console.log(room.num_user, room.max_user);
+    socket.join(roomID);
+    io.to(roomID).emit("update num_user", room.num_user, room.max_user);
+
+    if (room.canStart) {
+        io.to(roomID).emit("game start");
+        let question = room.questions[0];
+        io.to(roomID).emit("new question", question["definition"], question["options"]);
+    }
+
+
+    // FOR GAMING:
+
+    // send_question(socket);
     let htmlContent = '<html>\n<head>\n<title>Questions and Answers</title>\n</head>\n<body>\n';
     htmlContent += '<h1>Answer Logs</h1>\n'
+    
     socket.on("game clicked", function (data) {
         console.log(data);
         let res;
+        let userID = socket.request.session.userID, roomID = socket.request.session.roomID;
+        let room = manager.rooms[roomID], user = room.users[userID];
+
         // Determine whether the answer is correct or wrong.
-        if (data == questions[question_id]["correct_option"]) {    
+
+        if (data == room.questions[user.question_id]["correct_option"]) {    
             res = "correct";
-            htmlContent += `<p> <span>&#10004;</span> ${question_id}. ${questions[question_id]["definition"]} You answered: ${data}, which is correct.</p>\n`;
+            // htmlContent += `<p> <span>&#10004;</span> ${question_id}. ${questions[question_id]["definition"]} You answered: ${data}, which is correct.</p>\n`;
         }
         else {
             res = "wrong";
-            htmlContent += `<p> <span>&#10008;</span> ${question_id}. ${questions[question_id]["definition"]} You answered: ${data}. The correct answer was: ${questions[question_id]["correct_option"]}</p>\n`;
+            // htmlContent += `<p> <span>&#10008;</span> ${question_id}. ${questions[question_id]["definition"]} You answered: ${data}. The correct answer was: ${questions[question_id]["correct_option"]}</p>\n`;
         }
         console.log(res);
-        socket.emit("question result", res);
-        if (res == "correct") point_hp -= 10;
-        else squirrel_hp -= 20;
-        socket.emit("update hp", squirrel_hp, point_hp);
+        // socket.emit("question result", res);
+        if (res == "correct") {
+            room.boss_hp -= 10;
+            io.to(roomID).emit("update boss hp", room.boss_hp);
+        }
+        else {
+            user.hp -= 20;
+            socket.emit("update player hp", user.hp);
+        }
+        
         var counter = 0;
-        socket.emit("bounce back", counter);
-        if (squirrel_hp <= 0 || point_hp <= 0) {
+        // socket.emit("bounce back", counter);
+        if (user.hp <= 0 || room.boss_hp <= 0) {
             socket.disconnect();
             // Write questions and answers to a new HTML file
+            /*
             fs.writeFile("./public/wrong_answer.html", htmlContent, (err) => {
                 if (err) {
                     console.error(err);
@@ -216,17 +319,22 @@ io.sockets.on("connection", function (socket) {
                     console.log("Questions and answers written to questions.html");
                 }
             });
+            */
         }
-        question_id++;
-        send_question(socket);
+        user.question_id++;
+        send_question(socket, room, userID);
     });
+    
 
+
+    /*
     socket.on("timeout", function () {
-        squirrel_hp -= 20;
+        // squirrel_hp -= 20;
         socket.emit("update hp", squirrel_hp, point_hp);
-        question_id++;
-        send_question(socket);
+        // question_id++;
+        // send_question(socket);
     });
+    */
 
 });
 
@@ -246,6 +354,5 @@ app.get('/win_end', function (req, res) {
 app.get('/lose_end', function (req, res) {
     // Send gaming.html to client
     res.sendFile(__dirname + '/public/lose_end.html');
-
 
 });
